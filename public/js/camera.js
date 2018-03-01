ccb1e3fa347b897706b4a05279a3dba147bb952c
Zhaoshuo Bi <zbi@ucsd.com>
2018-03-01 01:47:44 -0800

    lab8

--- public/js/camera.js
+++ public/js/camera.js
@@ -1,11 +1,7 @@
 function initCamera() {
-  // Uncomment and fill in the correct selectors below.
-  // capture($('<Your code here>'),
-  //        $('<Your code here>'),
-  //        $('<Your code here>'));
-    capture($('#camera-video'),
-          $('#camera-canvas'),
-          $('#camera-button'));
+  capture($('#camera-video'),
+      $('#camera-canvas'),
+      $('#camera-button'));
 }
 
 function capture(video, canvas, snapshotButton) {
@@ -24,7 +20,7 @@ function capture(video, canvas, snapshotButton) {
         //Calculate dimension of photo from the video element.
         var width = video.width();
         var height = video.height();
-        
+
         canvas.attr('width', width);
         canvas.attr('height', height);
         ctx.drawImage(video[0], 0, 0, width, height);
@@ -42,4 +38,3 @@ function capture(video, canvas, snapshotButton) {
       successCallback, errorCallback);
 
 };
-
